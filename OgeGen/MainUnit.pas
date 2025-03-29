0b39214cde26fa1527edaf0c8a231e3ab92f4d7a
Безуглов Сергей <Besuglov.S@Gmail.com>
2025-03-29 14:54:33 +0400

    Switch from interop library to OpenXML

--- OgeGen/MainUnit.pas
+++ OgeGen/MainUnit.pas
@@ -1,19 +1,23 @@
-﻿unit MainUnit;
+﻿{#reference DocumentFormat.OpenXml.dll}
+
+Unit MainUnit;
 
 interface
 
-uses System, System.Drawing, System.Windows.Forms,
-  System.IO, Microsoft.Office.Interop.Word,
+uses System, System.IO, System.Drawing, System.Windows.Forms, 
+  DocumentFormat.OpenXml.Packaging,
+  DocumentFormat.OpenXml.Wordprocessing,
   oge01, oge02, oge03;
 
 type
   MainForm = class(Form)
-    procedure MainForm_Load(sender: Object; e: EventArgs);
     procedure browseSaveFolder_Click(sender: Object; e: EventArgs);
+    procedure MainForm_Load(sender: Object; e: EventArgs);
     procedure Save_Click(sender: Object; e: EventArgs);
   {$region FormDesigner}
   internal
     {$resource MainUnit.MainForm.resources}
+    groupBox1: GroupBox;
     task03Count: NumericUpDown;
     label3: &Label;
     task02Count: NumericUpDown;
@@ -23,13 +27,12 @@ type
     groupBox2: GroupBox;
     varsCount: NumericUpDown;
     groupBox3: GroupBox;
-    browseSaveFolder: Button;
     SaveFilename: TextBox;
     label5: &Label;
+    browseSaveFolder: Button;
     SaveFolder: TextBox;
     label4: &Label;
     Save: Button;
-    groupBox1: GroupBox;
     {$include MainUnit.MainForm.inc}
   {$endregion FormDesigner}
   public
@@ -38,6 +41,8 @@ type
       InitializeComponent;
     end;
   end;
+  
+  wText = DocumentFormat.OpenXml.Wordprocessing.Text;
 
 implementation
 
@@ -60,6 +65,35 @@ begin
   end;
 end;
 
+procedure AddPara(b: Body; str: string; fSize: integer; bld: boolean := False; 
+  just: string := '');
+begin
+  var p: Paragraph;
+  if just <> '' then
+  begin
+    var j := new Justification();    
+    if just = 'both' then
+      j.Val := JustificationValues.Both
+    else if just = 'center' then
+      j.Val := JustificationValues.Center;
+    var pp := new ParagraphProperties(j);
+    p := new Paragraph(pp);
+  end
+  else
+    p := new Paragraph();
+  // Добавляем параграф с текстом
+  var paragraph := b.AppendChild(p);
+  var run := paragraph.AppendChild(new Run());    
+  var runProperties := new RunProperties();
+  if bld then
+    runProperties.Bold := new Bold();  
+  
+  runProperties.FontSize := new FontSize();
+  runProperties.FontSize.Val := fSize.ToString; // 28-14; 32-16
+  run.AppendChild(runProperties);
+  run.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Text(str));  
+end;
+
 procedure MainForm.Save_Click(sender: Object; e: EventArgs);
 begin
   var varCount := integer(varsCount.Value);
@@ -71,140 +105,167 @@ begin
   var tasks01 := GenerateTasksOge01(varCount * t01Count);
   var tasks02 := GenerateTasksOge02(varCount * t02Count);
   var tasks03 := GenerateTasksOge03(varCount * t03Count);
-    
-  var wordApp := new ApplicationClass();
-  wordApp.Visible := false;
-  var wordDoc: _Document := wordApp.Documents.Add();
   
-  wordDoc.PageSetup.LeftMargin := wordApp.CentimetersToPoints(1);
-  wordDoc.PageSetup.RightMargin := wordApp.CentimetersToPoints(1);
-  wordDoc.PageSetup.TopMargin := wordApp.CentimetersToPoints(1);
-  wordDoc.PageSetup.BottomMargin := wordApp.CentimetersToPoints(1);
+  var savepath := SaveFolder.Text;
+  var filename := SaveFilename.Text;
+  var filePath := Path.Combine(savepath, filename);
+  
+  // Создаём документ
+  var doc := WordprocessingDocument.Create(
+    filePath,
+    DocumentFormat.OpenXml.WordprocessingDocumentType.Document);
+  
+  // Добавляем основную часть
+  var mainPart := doc.AddMainDocumentPart();
+  mainPart.Document := new Document();
+  var body := mainPart.Document.AppendChild(new Body());
+  
+  // Создаем секцию и устанавливаем поля
+  var sectionProperties := new SectionProperties();
+  
+  // Устанавливаем поля (1 см = 567 твипов)
+  var pageMargin := new PageMargin();
+  var x: longword := 567;
+  var x2: longword := 0;
+  pageMargin.Left := x;    // 1 см слева (567 twips = 1 см)
+  pageMargin.Right := x;   // 1 см справа
+  pageMargin.Top := 567;     // 1 см сверху
+  pageMargin.Bottom := 567;  // 1 см снизу
+  pageMargin.Header := x2;    // Верхний колонтитул
+  pageMargin.Footer := x2;    // Нижний колонтитул
+  pageMargin.Gutter := x2;    // Переплетный отступ
+  
+  sectionProperties.Append(pageMargin);
+  body.Append(sectionProperties);
+  
+  
   
   for var varNum := 0 to varCount - 1 do
   begin
-    var p := wordDoc.Paragraphs.Add();
-    p.Range.Text := 'Вариант № ' + (varNum + 1).ToString;
-    p.Range.Font.Size := 18;
-    p.Range.Font.Bold := 1;
-    p.Range.InsertParagraphAfter();
-    
     var taskNum := 1;
+    
+    AddPara(body, 'Вариант № ' + (varNum + 1).ToString, 36, true);
     
     for var i := 0 to t01Count - 1 do
     begin
-      var p2 := wordDoc.Paragraphs.Add();
-      p2.Range.Text := 'Задание № ' + taskNum.ToString + ' (01)';
-      p2.Format.Alignment := WdParagraphAlignment.wdAlignParagraphJustify;
-      p2.Range.Font.Size := 16;
-      p2.Range.Font.Bold := 1;
-      p2.Range.InsertParagraphAfter();
-      
-      var p3 := wordDoc.Paragraphs.Add();
-      p3.Range.Text := tasks01[varNum * t01Count + i][0];
-      p3.Format.Alignment := WdParagraphAlignment.wdAlignParagraphJustify;
-      p3.Range.Font.Size := 14;
-      p3.Range.Font.Bold := 0;
-      p3.Range.InsertParagraphAfter();
+      AddPara(body, 'Задание № ' + taskNum.ToString + ' (01)', 32, true);
+      AddPara(body, tasks01[varNum * t01Count + i][0], 28, false, 'both');      
       taskNum += 1;
     end;
     
     for var i := 0 to t02Count - 1 do
     begin
-      var p2 := wordDoc.Paragraphs.Add();
-      p2.Range.Text := 'Задание № ' + taskNum.ToString + ' (02)';
-      p2.Format.Alignment := WdParagraphAlignment.wdAlignParagraphJustify;
-      p2.Range.Font.Size := 16;
-      p2.Range.Font.Bold := 1;
-      p2.Range.InsertParagraphAfter();
-      
-      var p3 := wordDoc.Paragraphs.Add();
-      p3.Range.Text := tasks02[varNum * t02Count + i][0];
-      p3.Format.Alignment := WdParagraphAlignment.wdAlignParagraphJustify;
-      p3.Range.Font.Size := 14;
-      p3.Range.Font.Bold := 0;
-      p3.Range.InsertParagraphAfter();
+      AddPara(body, 'Задание № ' + taskNum.ToString + ' (02)', 32, true);
+      AddPara(body, tasks02[varNum * t02Count + i][0], 28, false, 'both');      
       taskNum += 1;
     end;
     
     for var i := 0 to t03Count - 1 do
     begin
-      var p2 := wordDoc.Paragraphs.Add();
-      p2.Range.Text := 'Задание № ' + taskNum.ToString + ' (03)';
-      p2.Format.Alignment := WdParagraphAlignment.wdAlignParagraphJustify;
-      p2.Range.Font.Size := 16;
-      p2.Range.Font.Bold := 1;
-      p2.Range.InsertParagraphAfter();
-      
-      var p3 := wordDoc.Paragraphs.Add();
-      p3.Range.Text := tasks03[varNum * t03Count + i][0];
-      p3.Format.Alignment := WdParagraphAlignment.wdAlignParagraphJustify;
-      p3.Range.Font.Size := 14;
-      p3.Range.Font.Bold := 0;
-      p3.Range.InsertParagraphAfter();
+      AddPara(body, 'Задание № ' + taskNum.ToString + ' (03)', 32, true);
+      AddPara(body, tasks03[varNum * t03Count + i][0], 28, false, 'both');      
       taskNum += 1;
     end;
     
-    var br: object := WdBreakType.wdPageBreak;
-    wordDoc.Words.Last.InsertBreak(br);
+    // Разрыв страницы
+    var breakPara := body.AppendChild(new Paragraph());
+    var breakRun := breakPara.AppendChild(new Run());
+    var br := new Break();
+    br.Type := BreakValues.Page;  
+    breakRun.AppendChild(br);
   end;
   
-  var p2 := wordDoc.Paragraphs.Add();
-  p2.Range.Text := 'ОТВЕТЫ';
-  p2.Format.Alignment := WdParagraphAlignment.wdAlignParagraphCenter;
-  p2.Range.Font.Size := 28;
-  p2.Range.Font.Bold := 0;
-  p2.Range.InsertParagraphAfter();
+  AddPara(body, 'ОТВЕТЫ', 40, true, 'center');
   
-  var range := wordDoc.Paragraphs.Last.Range;    
-  var table := wordDoc.Tables.Add(range, varCount + 1, varLength + 1);
-  table.Borders.Enable := 1;
+  // Создаем таблицу
+  var table := new Table();
   
-  table.Cell(1, 1).Range.Text := 'Вариант';
-  table.Cell(1, 1).Range.Font.Size := 12;
+  // Настройки свойств таблицы
+  var tw := new TableWidth(); 
+  tw.Width := '5000'; tw.Type := TableWidthUnitValues.Pct;
+  var u4: longword := 4;
+  var tb := new TopBorder(); tb.Val := BorderValues.Single; tb.Size := u4;
+  var bb := new BottomBorder(); bb.Val := BorderValues.Single; bb.Size := u4;
+  var lb := new LeftBorder(); lb.Val := BorderValues.Single; lb.Size := u4;
+  var rb := new RightBorder(); rb.Val := BorderValues.Single; rb.Size := u4;
+  var ihb := new InsideHorizontalBorder(); ihb.Val := BorderValues.Single; ihb.Size := u4;
+  var ivb := new InsideVerticalBorder(); ivb.Val := BorderValues.Single; ivb.Size := u4;
+  var borders := new TableBorders(tb, bb, lb, rb, ihb, ivb);
+  var tableProperties := new TableProperties(tw);
+  tableProperties.Append(borders);
+  table.AppendChild(tableProperties);
+  
+  // Создаем строки и ячейки
+  var row1 := new TableRow();
+  
+  var just := new Justification();
+  just.Val := JustificationValues.Center;
+  var ppCenter := new ParagraphProperties(just);
+  
+  
+  // Ячейка 1
+  var cell1 := new TableCell();  
+  cell1.Append(new Paragraph(ppCenter, new Run(new wText('Вариант'))));
+  row1.Append(cell1);
+  
   var taskStr := '01';
   for var i := 1 to varLength do
   begin
-    table.Cell(1, 1 + i).Range.Font.Size := 12;
-    table.Cell(1, 1 + i).Range.Text := $'№ {i} ({taskStr})';
+    just := new Justification();
+    just.Val := JustificationValues.Center;
+    ppCenter := new ParagraphProperties(just);
+    
+    var cell2 := new TableCell();
+    cell2.Append(new Paragraph(ppCenter, new Run(new wText($'№ {i} ({taskStr})'))));
+    row1.Append(cell2);
     if i = t01Count then taskStr := '02';
     if i = t01Count + t02Count then taskStr := '03';
   end;
+  table.Append(row1);
   
   for var i := 1 to varCount do
   begin
-    table.Cell(1 + i, 1).Range.Font.Size := 12;
-    table.Cell(1 + i, 1).Range.Text := i.ToString;
+    var row2 := new TableRow();
+    
+    just := new Justification();
+    just.Val := JustificationValues.Center;
+    ppCenter := new ParagraphProperties(just);
+    
+    row2.Append(new TableCell(new Paragraph(ppCenter, new Run(new wText(i.ToString)))));
+    
     for var j := 1 to varLength do
     begin
-      table.Cell(1 + i, 1 + j).Range.Font.Size := 12;
+      var lm := new LeftMargin(); 
+      lm.Width := '170'; lm.Type := TableWidthUnitValues.Dxa;
+      var tcp := new TableCellProperties(new TableCellMargin(lm));
+      
       if j <= t01Count then
       begin
-        var n := j;        
-        table.Cell(1 + i, 1 + j).Range.Text := tasks01[(i - 1) * t01Count + n - 1].Item2;        
+        var n := j;
+        row2.Append(new TableCell(tcp, new Paragraph(new Run(
+          new wText(tasks01[(i - 1) * t01Count + n - 1].Item2)))));        
       end
       else if j <= t01Count + t02Count then
       begin
-        var n := j - t01Count;
-        table.Cell(1 + i, 1 + j).Range.Text := tasks02[(i - 1) * t02Count + n - 1].Item2;
+        var n := j - t01Count;        
+        row2.Append(new TableCell(tcp, new Paragraph(new Run(
+          new wText(tasks02[(i - 1) * t02Count + n - 1].Item2)))));
       end
       else
       begin
         var n := j - t01Count - t02Count;
-        table.Cell(1 + i, 1 + j).Range.Text := tasks03[(i - 1) * t03Count + n - 1].Item2;
+        row2.Append(new TableCell(tcp, new Paragraph(new Run(
+          new wText(tasks03[(i - 1) * t03Count + n - 1].Item2)))));
       end;
     end;
+    table.Append(row2);
   end;
   
   
-  var savepath := SaveFolder.Text;
-  var filename := SaveFilename.Text;
-  var filePath := Path.Combine(savepath, filename);
-  var fileNameObj: object := filePath;
-  var formatObj: object := WdSaveFormat.wdFormatDocumentDefault;
-  wordDoc.SaveAs(fileNameObj, formatObj);
-  wordDoc.Close();
-  wordApp.Quit();
+  body.Append(table);
+  
+  // Закрываем документ
+  doc.Dispose();
 end;
 
 end.
